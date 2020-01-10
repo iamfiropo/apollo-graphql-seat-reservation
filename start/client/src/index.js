678a48122014b712127e41a027b05bb5c9198ebf
Johnpeace <princeolatjohnjoe@yahoo.com>
2020-01-10 21:37:43 +0100

    update data with mutations attach authorization headers to the request manage local state with apollo client cache

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -2,20 +2,46 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
-import React from "react";
+import gql from 'graphql-tag';
+import React, { useQuery } from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
+import Login from "./pages/login";
 import injectStyles from "./styles";
 
+import { resolvers, typeDefs } from "./resolvers";
+
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'http://localhost:4000/'
-});
 
 const client = new ApolloClient({
   cache,
-  link
+  link: new HttpLink({
+
+    headers: { authorization: localStorage.getItem('token') },
+    uri: "http://localhost:4000/graphql",
+  }),
+  typeDefs,
+  resolvers
 });
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 injectStyles();
 ReactDOM.render(
